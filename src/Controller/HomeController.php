9743d0dccb1d2a8a09b41df480dc81122c623b18
stephane.ardin@gmail.com <stephane.ardin@gmail.com>
2020-01-15 23:44:40 +0100

    mise en place d'un carousel

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Provider\ProjectProvider;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +10,25 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
+     * @var ProjectProvider
+     */
+    private $projectProvider;
+
+    public function __construct(
+        ProjectProvider $projectProvider
+    ) {
+        $this->projectProvider = $projectProvider;
+    }
+
+    /**
      * @Route("/", name="home")
      */
     public function index():Response
     {
-        return $this->render('home/home.html.twig', []);
-    }
+        $list = $this->projectProvider->provideAll();
+        return $this->render('home/home.html.twig', [
+            'projectList' => $list
+        ]);    }
     /**
      * @Route("/contact", name="contact")
      */
